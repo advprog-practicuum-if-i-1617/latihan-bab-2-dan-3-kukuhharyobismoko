4dfd88ec58f611be453cf6991b316dd39a8f50ee
kukuhharyobismoko <kadalmbesetggwp@gmail.com>
2017-03-23 23:13:58 +0700

    Fixing method konversi huruf ke angka
    fix utama method konversi huruf ke angka , namun ada beberapa fixing di
    bagian kelas main dan lainnya tapi cuma dikit hehe

--- src/latihan/Convert.java
+++ src/latihan/Convert.java
@@ -1,6 +1,9 @@
 package latihan;
+
 public class Convert {
+
     public static String[] Data = {"", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"};
+
     public static String change(int a) {
         if (a < 12) {
             return Data[a];
@@ -20,9 +23,10 @@ public class Convert {
         if (a == 1000) {
             return "Seribu ";
         }
-        return "";
+        return "1-1000 ya :) ";
     }
-    public static int change(String b){
+
+    public static int change(String b) {
         for (int i = 0; i < 12; i++) {
             if (b.equalsIgnoreCase(Data[i])) {
                 return i;
@@ -35,12 +39,29 @@ public class Convert {
                 if (b.equalsIgnoreCase(Data[i] + " puluh " + Data[j])) {
                     String sc = Integer.toString(i) + Integer.toString(j);
                     return Integer.parseInt(sc);
+                } else if (b.equalsIgnoreCase("seratus " + Data[j])) {
+                    return Integer.parseInt("10" + j);
+                } else if (b.equalsIgnoreCase("seratus " + Data[i] + " puluh")) {
+                    return Integer.parseInt("1" + i + "0");
+                } else if (b.equalsIgnoreCase("seratus " + Data[i] + " puluh " + Data[j])) {
+                    return Integer.parseInt("1" + i + j);
+                } else if (b.equalsIgnoreCase(Data[i] + " ratus")) {
+                    return Integer.parseInt(i + "00");
+                } else if (b.equalsIgnoreCase(Data[i] + " ratus " + Data[j])) {
+                    return Integer.parseInt(i + "0" + j);
+                } else if (b.equalsIgnoreCase(Data[i] + " ratus " + Data[j] + " puluh")) {
+                    return Integer.parseInt(i + "" + j + "0");
                 }
-            if (b.equalsIgnoreCase("Seribu")) {
-                return 1000;
+                for (int k = 0; k < 10; k++) {
+                    if (b.equalsIgnoreCase(Data[i] + " ratus " + Data[j] + " puluh " + Data[k])) {
+                        return Integer.parseInt(i + "" + j + "" + k);
+                    }
+                }
+                if (b.equalsIgnoreCase("Seribu")) {
+                    return 1000;
+                }
             }
         }
-        
-    }return 999999;
-}
+        return 999999;
+    }
 }
--- src/latihan/Main.java
+++ src/latihan/Main.java
@@ -12,20 +12,26 @@ import java.util.Scanner;
  * @author kukuh
  */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("1.Angka -> Huruf");
-        System.out.println("2.Huruf -> Angka");
-        System.out.print("Pilih : ");
-        Scanner sc=new Scanner(System.in);
-        int pilih=sc.nextInt();
-        if (pilih==1) {
-            System.out.print("Masukkan angka : ");int angka=sc.nextInt();
-            System.out.println(Convert.change(angka));
-        }else if (pilih==2) {
-            System.out.print("Masukkan huruf : ");
-            String huruf=sc.nextLine();
-            huruf=sc.nextLine();
-            System.out.println(Convert.change(huruf));
-        }
+        Scanner sc = new Scanner(System.in);
+        int pilih;
+        do {
+            System.out.println("1.Angka -> Huruf");
+            System.out.println("2.Huruf -> Angka");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih : ");
+            pilih = sc.nextInt();
+            if (pilih == 1) {
+                System.out.print("Masukkan angka : ");
+                int angka = sc.nextInt();
+                System.out.println(Convert.change(angka));
+            } else if (pilih == 2) {
+                System.out.print("Masukkan huruf : ");
+                String huruf = sc.nextLine();
+                huruf = sc.nextLine();
+                System.out.println(Convert.change(huruf));
+            }
+        } while (pilih != 0);
     }
 }
